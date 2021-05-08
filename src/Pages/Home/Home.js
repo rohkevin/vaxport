d18892e06baa6c1781c9f03a0400732c600201a8
rohkevin <kevinrohmail@gmail.com>
2021-05-08 11:00:30 -0400

    Install SASS, create temporary styling elements using theme and index scss

--- src/Pages/Home/Home.js
+++ src/Pages/Home/Home.js
@@ -1,10 +1,19 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
+
+import './Home.scss'
 
 function Home() {
   return (
-    <div>
-      
-    </div>
+    <main>
+      <div className="header">
+        <h1>Vaxport</h1>
+      </div>
+      <div className="signup">
+
+        <button className="home-btn">Sign Up</button>
+      </div>
+    </main>
   )
 }
 
