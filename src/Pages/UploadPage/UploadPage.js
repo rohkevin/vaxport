9665a8d3559ba5d0dc2ec2cf962413d5259ed4c7
rohkevin <kevinrohmail@gmail.com>
2021-05-08 11:57:21 -0400

    Prep for temp reroute for skeleton

--- src/Pages/UploadPage/UploadPage.js
+++ src/Pages/UploadPage/UploadPage.js
@@ -1,9 +1,16 @@
 import React from 'react'
 
 function UploadPage() {
+
+  const handleUpload = () => {
+    // If upload successful display success message 
+    // Temporary reroute
+  }
   return (
     <main>
       <h1 className="h2">Upload Documents</h1>
+      <p>Please upload a clear picture of your vaccination record.</p>
+      <button type="button" onClick={handleUpload}>Upload image</button>
       
     </main>
   )
