5f8b83b85ff5cfb98fd7c921a121733b28e917b3
rohkevin <kevinrohmail@gmail.com>
2021-05-08 22:15:25 -0400

    Update styling for home page

--- src/Pages/Home/Home.js
+++ src/Pages/Home/Home.js
@@ -9,8 +9,17 @@ function Home() {
       <div className="header">
         <h1 className="logo">Vaxport</h1>
       </div>
-      <Link to="/login"><button className="home-btn">GET STARTED</button></Link>
-      <Link to="/login"><button className="button-2">Sign Up</button></Link>
+      <div className="home-intro">
+        <h2>Say hello to Vaxport! Globally approved Covid-19 vaccine certification passport platform.</h2>
+        <p>We get rid of struggles proving your vaccination record. Make your business trip and vacation more seamlessly</p>
+
+        <Link to="/login"><button className="home-btn">GET STARTED</button></Link>
+        <Link to="/login"><button className="button-2">Sign Up</button></Link>
+      </div>
+      <div className="home-extra">
+        <h3>How it works</h3>
+
+      </div>
     </main>
   )
 }
