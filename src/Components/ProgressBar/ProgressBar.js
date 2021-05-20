a5d1bb6934b22bf2aeeb10190af4e2bde0e6b7b2
rohkevin <kevinrohmail@gmail.com>
2021-05-20 11:52:44 -0400

    Disable setTimeout which was causing memory leak

--- src/Components/ProgressBar/ProgressBar.js
+++ src/Components/ProgressBar/ProgressBar.js
@@ -4,14 +4,15 @@ import './ProgressBar.scss'
 const ProgressBar = ({done}) => {
 	const [style, setStyle] = useState({});
 	
-	setTimeout(() => {
-		const newStyle = {
-			opacity: 1,
-			width: `${done}%`
-		}
+	// setTimeout(() => {
+	// 	const newStyle = {
+	// 		opacity: 1,
+	// 		width: `${done}%`
+	// 	}
 		
-		setStyle(newStyle);
-	}, 200);
+	// 	setStyle(newStyle);
+  //   console.log('progressbar');
+	// }, 2000);
 	
 	return (
 		<div className="progress">
