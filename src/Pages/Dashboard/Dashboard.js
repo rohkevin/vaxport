e3ce02f9600aaa9ad5c8006b5b7ac26113d4a156
rohkevin <kevinrohmail@gmail.com>
2021-05-22 22:34:08 -0400

    Render user data for dashboard, preparation for news api component

--- src/Pages/Dashboard/Dashboard.js
+++ src/Pages/Dashboard/Dashboard.js
@@ -13,43 +13,51 @@ function Dashboard() {
   useEffect(() => {
     if (currentUser) {
       db.collection("users").doc(currentUser.email).get().then((doc) => {
-        const { firstName, lastName, email, passportNumber, recordURL } = doc.data();
+        const userData = doc.data();
+        const { firstName, lastName, email, passportNumber } = userData;
+        var governmentVerified = null;
 
+        if ("governmentVerified" in userData) {
+          governmentVerified = userData.governmentVerified
+        }
+        setUser({
+          name: firstName + ' ' + lastName,
+          email: email,
+          passport: passportNumber,
+          governmentVerified: governmentVerified
+        })
       })
 
     }
   }, [])
-
-  return (
-    <main id="dashboard">
-      <div className="page-wrapper">
-        <h1>My Account</h1>
-        <h3>Legal Name</h3>
-        {/* Generated from user id */}
-        <div className="info-box">
-          Kevin Roh
-        </div>
-        <h3>Email</h3>
-        <div className="info-box">
-          kev@kev.com
-        </div>
-        <h3>Passport Information</h3>
-        <div className="info-box">
-          A1A1A1A1
-        </div>
-        <h3>Vaccination Records</h3>
-        <div className="info-box">
-          <p>Vaccination: COVID-19</p>
-          <p>Received: Pfizer</p>
-          <p>1st Dose Received: 10/05/2021</p>
-          <p>2nd Dose Received: 10/09/2021</p>
-          <p></p>
-
-        </div>
-        <button type="button" onClick={() => history.push("/QRCode")} className="button-2">View My QR Badge</button>
-      </div>
-    </main>
-  )
+  if (user) {
+    return (
+      <main id="dashboard">
+        <h2>Welcome back</h2>
+        <h3>{user.name}</h3>
+        <button 
+          type="button" 
+          className="button"
+          >
+          View my documents
+        </button>
+        <button 
+          type="button" 
+          onClick={() => history.push("/QRCode")} 
+          className="button"
+          >
+          View My QR Badge
+        </button>
+        <LatestNews />
+      </main>
+    )
+  } else {
+    return (
+      <main id="dashboard">
+        <h2>No user</h2>
+      </main>
+    )
+  }
 }
 
 export default Dashboard
